4a0bc138ec069e391de0ad548ad47d4c5548ca94
Lukas Kozak <LukasKozak88@gmail.com>
2020-09-07 13:51:34 +0200

    vytvoreni_postpresenteru_pripojeni_k_databazi_vypsani_konkretniho_prispevku

--- app/Presenters/PostPresenter.php
+++ app/Presenters/PostPresenter.php
@@ -1,22 +1,21 @@
-<?php
+<?php //vytvoříme samostatný presenter pro POST (PostPresenter.php), v něm použijeme constructor, který nás připojí k databázi a metodu renderShow, která nám pomůže zobrazit jeden konkrétní příspěvek z databáze
 
-namespace App\Presenters;
+namespace App\Presenters; //POZOR na uvedení správného namespacu!
 
 use Nette;
 use Nette\Application\UI\Form;
 
-
-class PostPresenter extends Nette\Application\UI\Presenter
+class PostPresenter extends Nette\Application\UI\Presenters
 {
 	/** @var Nette\Database\Context */
 	private $database;
 
-	public function __construct(Nette\Database\Context $database)
+	public function __construct (Nette\Database\Context $database)
 	{
 		$this->database = $database;
 	}
 
-	public function renderShow(int $postId): void
+	public function renderShow(int $postId): void //tato metoda vyžaduje jeden argument - ID jednoho konkrétního článku
 	{
 		$this->template->post = $this->database->table('posts')->get($postId);
 	}
